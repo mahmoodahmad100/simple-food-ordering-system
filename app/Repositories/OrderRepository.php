3bcdf3d9adad113126fcb92afea822ff0b85e2c0
Mahmood Ahmad <mahmood073@gmail.com>
2025-08-13 19:38:29 +0300

    order repo: create logic

--- app/Repositories/OrderRepository.php
+++ app/Repositories/OrderRepository.php
@@ -2,7 +2,26 @@
 
 namespace App\Repositories;
 
+use App\Models\Order;
+
 class OrderRepository
 {
-    //...
+    /**
+     * @param Order $model
+     */
+    public function __construct(private Order $model)
+    {
+
+    }
+
+    /**
+     * create new order
+     * 
+     * @param array $data
+     * @return Order
+     */
+    public function create(array $data): Order
+    {
+        return $this->model->create($data);
+    }
 }

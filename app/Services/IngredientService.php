e5a1ae1b5fa94dc188214eda29e35ac5af8ad8b1
Mahmood Ahmad <mahmood073@gmail.com>
2025-08-14 12:22:21 +0300

    initial logic of the order and ingredient services

--- app/Services/IngredientService.php
+++ app/Services/IngredientService.php
@@ -2,7 +2,37 @@
 
 namespace App\Services;
 
+use App\Models\Order;
+
 class IngredientService
 {
-    //...
+    /**
+     * @param Order $order
+     * @return void
+     */
+    public function updateStock(Order $order): void
+    {
+        foreach ($order->products as $product) {
+            foreach ($product->ingredients as $ingredient) {
+                $ingredient->current_amount -= $product->pivot->quantity * $ingredient->pivot->amount;
+
+                if ($ingredient->current_amount < 0) {
+                    abort(422, "Not enough ingredients for {$product->name}({$ingredient->name} is out of stock)");
+                }
+
+                if ($ingredient->isDirty('current_amount')) {
+                    $threshold = config('app.stock_threshold_percentage');
+                    if (((($ingredient->current_amount / $ingredient->total_amount) * 100) < $threshold) &&
+                        !$ingredient->is_low_amount_alert_email_sent) {
+                        /**
+                         * @TODO: Send email to merchant
+                         */
+                        $ingredient->is_low_amount_alert_email_sent = true;
+                    }
+                }
+
+                $ingredient->save();
+            }
+        }
+    }
 }
--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -2,7 +2,58 @@
 
 namespace App\Services;
 
+use App\Repositories\OrderRepository;
+use App\Services\IngredientService;
+use App\Models\Order;
+use Illuminate\Support\Facades\DB;
+
 class OrderService
 {
-    //...
+    /**
+     * @param OrderRepository $orderRepository
+     * @param IngredientService $ingredientService
+     */
+    public function __construct(
+        private OrderRepository $orderRepository,
+        private IngredientService $ingredientService
+    )
+    {
+        //...
+    }
+
+    /**
+     * @param array $products
+     * @return array
+     */
+    public function getFormattedProducts(array $products): array
+    {
+        $result = [];
+
+        foreach ($products as $product) {
+            $result[$product['product_id']] = ['quantity' => $product['quantity']];
+        }
+
+        return $result;
+    }
+
+    /**
+     * This method is used to create an order and update the ingredients stock.
+     * Please note that it is wrapped in a transaction to ensure that:
+     * 1. the order is created and the ingredients stock is updated.
+     * 2. if the ingredients stock is not enough, the order is not created.
+     * so the order will not be created if the ingredients stock is not enough.
+     * and that's why the code below is wrapped in a transaction to ensure consistency.
+     * 
+     * @param array $data
+     * @return Order
+     */
+    public function create(array $data): Order
+    {
+        return DB::transaction(function () use ($data) {
+            $order = $this->orderRepository->create($data);
+            $order->products()->sync($this->getFormattedProducts($data['products']));
+            $this->ingredientService->updateStock($order);
+            return $order;
+        });
+    }
 }
